9a5bb4e1982e82972d10288714ebd263d1a71f88
Max <karatemaksim0110@gmail.com>
2025-04-21 08:23:34 +0300

    изменил часть проги которая отвечает за переработку текста

--- src/TextPreparer/text_preparer.cpp
+++ src/TextPreparer/text_preparer.cpp
@@ -3,25 +3,24 @@
 
 #include "text_preparer.h"
 
-PreparingFuncStatus TextPrepare (const char *input_file_name) {
-
+PreparingFuncStatus prepare_text (const char* input_file_name) 
+{
     assert (input_file_name);
 
-    FILE *input_file = fopen (input_file_name, "r");
-
-    if (!input_file) {
-
-        fprintf (stderr, "TextPrepare: No file matching the name \"%s\" was found.\n", input_file_name);
+    FILE* input_file = fopen (input_file_name, "r");
+    if  (!input_file) 
+    {
+        fprintf (stderr, "%s: No file matching the name \"%s\" was found.\n", __func__, input_file_name);
         return PREPARING_FUNC_STATUS_FAIL;
     }
 
-    FILE *output_file = fopen (TextPreparedFileNameGen (input_file_name), "w");
+    FILE*   output_file = fopen (generate_prepared_filename (input_file_name), "w");
     assert (output_file);
 
     char word[MAX_WORD_LENGTH] = {};
 
-    for (; !feof (input_file);) {
-
+    for (; !feof (input_file);) 
+    {
         int fscanf_status = fscanf (input_file, " %[^-;,.\"*:?\n\r!() ]", word); 
 
         if (fscanf_status != EOF && fscanf_status != 0)
@@ -40,8 +39,8 @@ PreparingFuncStatus TextPrepare (const char *input_file_name) {
     return PREPARING_FUNC_STATUS_OK;
 }
 
-const char *TextPreparedFileNameGen (const char *input_file_name) {
-
+const char* generate_prepared_filename (const char* input_file_name) 
+{
     assert (input_file_name);
 
     static char file_name[TXT_PREP_MAX_FILE_NAME_LENGTH] = {};
--- src/TextPreparer/text_preparer.h
+++ src/TextPreparer/text_preparer.h
@@ -5,14 +5,14 @@ const int TXT_PREP_MAX_FILE_NAME_LENGTH = 64;
 
 const int MAX_WORD_LENGTH = 32;
 
-enum PreparingFuncStatus {
-
+enum PreparingFuncStatus 
+{
     PREPARING_FUNC_STATUS_OK,
     PREPARING_FUNC_STATUS_FAIL
 };
 
-PreparingFuncStatus TextPrepare (const char *input_file_name);
+PreparingFuncStatus prepare_text              (const char* input_file_name);
+const char*        generate_prepared_filename (const char* input_file_name);
 
-const char *TextPreparedFileNameGen (const char *input_file_name);
 
 #endif
